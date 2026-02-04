cc0477acc5b2799f89735983ad2bae5504d6d81f
alexisgalvis1996-commits <alexisgalvis1996@gmail.com>
2026-02-04 17:54:34 -0500

    update reository

--- ContactosAPI/obj/Debug/net8.0/ContactosAPI.AssemblyInfo.cs
+++ ContactosAPI/obj/Debug/net8.0/ContactosAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+892e3ba938c3ce0b7b6287aae322d7ff3101624a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
