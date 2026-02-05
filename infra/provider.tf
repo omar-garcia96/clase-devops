130caea3c80fc113527e37275986033027cab1ea
alexisgalvis1996-commits <alexisgalvis1996@gmail.com>
2026-02-05 18:31:54 -0500

    add infra

---
+++ infra/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "6fd83fb0-ca8a-4913-9403-facfba8afedd"
+}
